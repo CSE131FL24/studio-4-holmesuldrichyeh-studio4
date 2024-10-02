72e1e885154bf2390b73f09ef020ad9900e3829c
holme <holme@MaxPC.dhcp.wustl.edu>
2024-10-02 13:40:54 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,32 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
-	}
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.filledSquare(0.3, 0.5, 0.2);
+		StdDraw.filledSquare(.7, 0.5, 0.2);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.filledCircle(0.35, 0.6, 0.05);
+		StdDraw.filledCircle(0.65, 0.6, 0.05);
+		StdDraw.filledSquare(.4, 0.4, 0.02);
+		StdDraw.filledSquare(.44, 0.4, 0.02);
+		StdDraw.filledSquare(.48, 0.4, 0.02);
+		StdDraw.filledSquare(.52, 0.4, 0.02);
+		StdDraw.filledSquare(.56, 0.4, 0.02);
+		StdDraw.filledSquare(.60, 0.4, 0.02);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.setPenRadius(0.05);
+		StdDraw.filledSquare(0.14, 0.74, 0.04);
+		StdDraw.filledSquare(0.22, 0.74, 0.04);
+		StdDraw.filledSquare(0.3, 0.74, 0.04);
+		StdDraw.filledSquare(0.38, 0.74, 0.04);
+		StdDraw.filledSquare(0.46, 0.74, 0.04);
+		StdDraw.filledSquare(0.52, 0.74, 0.04);
+		StdDraw.filledSquare(0.6, 0.74, 0.04);
+		StdDraw.filledSquare(0.68, 0.74, 0.04);
+		StdDraw.filledSquare(0.76, 0.74, 0.04);
+		StdDraw.filledSquare(0.84, 0.74, 0.04);
+		StdDraw.filledSquare(0.86, 0.74, 0.04);
+	}	
 }
